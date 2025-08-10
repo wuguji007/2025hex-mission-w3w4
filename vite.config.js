1c09a144e9bc9224cc18891a62d3f079ca9f8ba8
wuguji007 <alie40521@gmail.com>
2025-08-10 19:48:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/2025hex-mission-w3w4/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
